d22cc0295191ff76a2433b6dc84e87ca12d58e9e
darreljames <dcastellano2007@gmail.com>
2019-11-14 21:06:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let m = document.getElementById('main');
+m.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "Darrel is the champion"
