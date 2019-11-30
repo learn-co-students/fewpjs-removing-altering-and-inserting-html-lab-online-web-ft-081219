b0908af80d6638ec02f66ec92865c7b47700466f
Emilia <emilia.mena26@gmail.com>
2019-11-30 16:59:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+//document.querySelector("main#main").remove();
+main.remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+
+const name = "Emi"
+newHeader.innerHTML = `${name} is the champion!`
