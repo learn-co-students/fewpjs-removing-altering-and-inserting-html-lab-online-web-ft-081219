31092e2f2f55bd9a38906ba056491070fa3753ab
github username <github email address>
2020-01-25 01:55:59 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+main.remove()
+    
+// let newHeader = document.getElementById("h1#victory")
+
+
+
+    let newHeader = document.createElement('h1')
+
+   
+    
+  newHeader.id = 'victory'
+// let newHeader = document.createElement('h1')
+//   document.body.appendChild(newHeader)
+// let h1 = document.querySelector("h1#victory");
+newHeader.innerHTML = 'Chyrelle is the champion'
